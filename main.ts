820afdadb57b98e309928fe17f9b3d50a5053859
nano <learnwithmrnano@gmail.com>
2024-08-27 22:09:25 +0530

    commit - remove unnecessary funtions and fix regex to take only the last ##Dates

--- main.ts
+++ main.ts
@@ -1,88 +1,16 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Notice, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
 export default class MarkAsReviewedPlugin extends Plugin {
-	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
 		this.addCommand({
 			id: 'mark-note-as-reviewed',
 			name: 'Mark Note as Reviewed',
 			callback: () => this.markAsReviewed(),
 		});
 		this.addButton('header');
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		
 	}
 
 	async markAsReviewed() {
@@ -107,13 +35,15 @@ export default class MarkAsReviewedPlugin extends Plugin {
 		});
 
 		const reviewedLine = `- Reviewed on [[${dateStr}]] at ${timeStr}`;
-		const dateHeadingRegex = /^(#+ ?Dates?)$/m;
-		const dateHeadingMatch = content.match(dateHeadingRegex);
-
+		const dateHeadingRegex = /^(#+ ?Dates?)$/gm;
+		const matches = [...content.matchAll(dateHeadingRegex)];
+		
 		let updatedContent;
-		if (dateHeadingMatch) {
-			const [fullMatch] = dateHeadingMatch;
-			updatedContent = content.replace(fullMatch, `${fullMatch}\n${reviewedLine}`);
+		if (matches.length > 0) {
+			const lastMatch = matches[matches.length - 1];
+			const index = lastMatch?.index ?? content.length;
+			updatedContent = content.slice(0, index) + 
+							content.slice(index).replace(lastMatch?.[0] ?? '', `${lastMatch?.[0] ?? ''}\n${reviewedLine}`);
 		} else {
 			updatedContent = `${content}\n\n## Dates\n${reviewedLine}`;
 		}
@@ -151,53 +81,5 @@ export default class MarkAsReviewedPlugin extends Plugin {
 		console.log('Unloading Mark as Reviewed Plugin');
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MarkAsReviewedPlugin;
-
-	constructor(app: App, plugin: MarkAsReviewedPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	
 }
